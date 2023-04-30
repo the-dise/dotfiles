057cde4f35ceb0a062b6803faddfe7a75546df10
the_dise <31319017+the-dise@users.noreply.github.com>
2023-04-30 14:30:45 +0300

    upd profile.ps1

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 Set-PSReadLineOption -PredictionSource History
 
-oh-my-posh init pwsh --config "C:\Users\dmitr\AppData\Local\Programs\oh-my-posh\themes\gruvbox.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "D:\Documents\WindowsPowerShell\dbox.omp.json" | Invoke-Expression
 
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
