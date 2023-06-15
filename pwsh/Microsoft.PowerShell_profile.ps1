d4853ab7f9ecc5f93d65f2b3d2d534e58ff090ba
the_dise <dmitry.dise.gavrilov@gmail.com>
2023-06-15 11:43:53 +0300

    add icon for tabs

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 Set-PSReadLineOption -PredictionSource History
 
-oh-my-posh init pwsh --config "D:\Documents\WindowsPowerShell\dbox.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "D:\Documents\PowerShell\dbox.omp.json" | Invoke-Expression
 
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
