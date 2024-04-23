6ee8ac0e66ded7e778c4b10fcb85ed2056b27ffd
the_dise <dmitry.dise.gavrilov@gmail.com>
2024-04-23 19:40:36 +0300

    added completion

---
+++ zsh/completion.zsh
@@ -0,0 +1,7 @@
+# vim: ft=zsh
+
+# matches case insensitive for lowercase
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# pasting with tabs doesn't perform completion
+zstyle ':completion:*' insert-tab pending
