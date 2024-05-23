5eef791b01c920271d68e99f08ef49bc4444d7d7
the_dise <dmitry.dise.gavrilov@gmail.com>
2024-05-23 19:52:44 +0300

    Optimized alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,41 +1,49 @@
 # vim: ft=zsh
+
+# Exit shell
 alias :q="exit"
 
-# General
-alias rm='rm -i'
+# General file operations
 alias cp='cp -i'
 alias mv='mv -i'
 alias vi='nvim'
 
-# Clipboard
+# Clipboard operations
 alias copy='xsel --clipboard --input'
 alias paste='xsel --clipboard --output'
 
-# yt-dlp
+# yt-dlp commands
 alias yt='yt-dlp --add-metadata -ic' # Download video
 alias yta='yt-dlp --add-metadata -xic' # Download audio
 
-# tmux sessions
+# Tmux sessions
 tas() {
     tmux attach-session -t "$1"
 }
+_complete_sessions() {
+    local sessions
+    sessions=$(tmux list-sessions -F "#S" 2>/dev/null)
+    COMPREPLY=($(compgen -W "$sessions" -- "${COMP_WORDS[COMP_CWORD]}"))
+}
+complete -F _complete_sessions tas
+
 tns() {
     tmux new-session -s "$1"
 }
 
 # Package management
 alias up="sudo dnf upgrade --refresh --best --allowerasing -y && flatpak update -y"
-alias cc="sudo dnf autoremove && dnf clean all && flatpak uninstall --unused -y && flatpak remove --delete-data && sudo journalctl --vacuum-time=1weeks"
+alias cc="sudo dnf autoremove && sudo dnf clean all && flatpak uninstall --unused -y && flatpak remove --delete-data && sudo journalctl --vacuum-time=1weeks"
 
-# tarball
+# Tarball extraction
 alias tarball="tar -C ~/Applications -xvf"
 
-# Etcx
+# Miscellaneous
 alias c="clear"
 alias sn="sudo nautilus"
 
-# Neofetch
-alias n="neofetch"    
+# System info
+alias n="neofetch"
 alias k="uname -rs"
 alias age="stat / | grep Birth"
 
@@ -52,13 +60,7 @@ usb() {
 }
 
 # Fzf with ripgrep
-fzf-rg() {
-    local query="$1"
-    rg --files-with-matches --no-heading --line-number --color=always "$query" | fzf --ansi --preview="rg --color=always -n $query {1}" --bind "enter:execute(rg --color=always -n $query {1})"
-}
-
-_complete_fzf_rg() {
-    COMPREPLY=()
-}
-
-complete -F _complete_fzf_rg fzf-rg
+alias fzf-rg='
+    fzf-rg-helper() {
+        rg --files-with-matches --no-heading --line-number --color=always "$1" | fzf --ansi --preview="rg --color=always -n $1 {1}" --bind "enter:execute(rg --color=always -n $1 {1})"; 
+    }; fzf-rg-helper'
