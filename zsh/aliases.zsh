07edf1d4c13386cdc7e33cf111776a84383d2e7e
the_dise <dmitry.dise.gavrilov@gmail.com>
2024-06-24 13:33:28 +0300

    added dnf aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -41,6 +41,26 @@ tns() {
 alias up="sudo dnf upgrade --refresh --best --allowerasing -y && flatpak update -y"
 alias cc="sudo dnf autoremove && sudo dnf clean all && flatpak uninstall --unused -y && flatpak remove --delete-data && sudo journalctl --vacuum-time=1weeks"
 
+### dnf aliases
+local dnfprog="dnf"
+
+# Prefer dnf5 if installed
+command -v dnf5 > /dev/null && dnfprog=dnf5
+
+alias dnfl="${dnfprog} list"                       # List packages
+alias dnfli="${dnfprog} list installed"            # List installed packages
+alias dnfgl="${dnfprog} grouplist"                 # List package groups
+alias dnfmc="${dnfprog} makecache"                 # Generate metadata cache
+alias dnfp="${dnfprog} info"                       # Show package information
+alias dnfs="${dnfprog} search"                     # Search package
+
+alias dnfu="sudo ${dnfprog} upgrade"               # Upgrade package
+alias dnfi="sudo ${dnfprog} install"               # Install package
+alias dnfgi="sudo ${dnfprog} groupinstall"         # Install package group
+alias dnfr="sudo ${dnfprog} remove"                # Remove package
+alias dnfgr="sudo ${dnfprog} groupremove"          # Remove package group
+alias dnfc="sudo ${dnfprog} clean all"             # Clean cache
+
 # Tarball extraction
 alias tarball="tar -C ~/Develop/Apps -xvf"
 
