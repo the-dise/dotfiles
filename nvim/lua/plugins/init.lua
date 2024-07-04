932ab83a887518112fa63a913a06a53ff8969d60
the_dise <dmitry.dise.gavrilov@gmail.com>
2024-07-04 16:28:56 +0300

    [nvim] init commit

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false
+  },
+  -- {
+  --   "mg979/vim-visual-multy",
+  --   branch = "master",
+  --   init = function ()
+  --     vim.g.VM_maps = {
+  --       ["Find Under"] = "<C-n>"
+  --     }
+  --   end,
+  -- }
+  -- These are some examples, uncomment them if you want to see them work!
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     require("nvchad.configs.lspconfig").defaults()
+  --     require "configs.lspconfig"
+  --   end,
+  -- },
+  --
+  -- {
+  -- 	"williamboman/mason.nvim",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"lua-language-server", "stylua",
+  -- 			"html-lsp", "css-lsp" , "prettier"
+  -- 		},
+  -- 	},
+  -- },
+  --
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
