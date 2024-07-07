2dcdc6a8c9075ceaba9b1da29e88c358b1e6dac8
the_dise <dmitry.dise.gavrilov@gmail.com>
2024-07-07 20:21:27 +0300

    [nvim] added comments to mapping

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,16 +1,19 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+-- quick file saving via ctrl+s
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
+-- vim like window switcher
 map({"n", "v"}, "<c-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "Windows Left"})
 map({"n", "v"}, "<c-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "Windows Right"})
 map({"n", "v"}, "<c-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "Windows Down"})
 map({"n", "v"}, "<c-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "Windows Up"})
+
+-- home/end
 map({"n", "v"}, "L", "$", { desc = "End of line" })
 map({"n", "v"}, "H", "^", { desc = "Start of line" })
