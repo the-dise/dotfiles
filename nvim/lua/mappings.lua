f2826cd3e3aa74472cdf09f47b8b684e867a619f
the_dise <dmitry.dise.gavrilov@gmail.com>
2024-07-10 10:27:12 +0300

    [nvim] prettier mapping.lua

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -9,11 +9,12 @@ map("i", "jk", "<ESC>")
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 -- vim like window switcher
-map({"n", "v"}, "<c-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "Windows Left"})
-map({"n", "v"}, "<c-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "Windows Right"})
-map({"n", "v"}, "<c-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "Windows Down"})
-map({"n", "v"}, "<c-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "Windows Up"})
+map({ "n", "v" }, "<c-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "Windows Left" })
+map({ "n", "v" }, "<c-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "Windows Right" })
+map({ "n", "v" }, "<c-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "Windows Down" })
+map({ "n", "v" }, "<c-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "Windows Up" })
 
 -- home/end
-map({"n", "v"}, "L", "$", { desc = "End of line" })
-map({"n", "v"}, "H", "^", { desc = "Start of line" })
+map({ "n", "v" }, "L", "$", { desc = "End of line" })
+map({ "n", "v" }, "H", "^", { desc = "Start of line" })
+
