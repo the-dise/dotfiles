5cccaba1d41b2f7d88d44d67780ccc2ef485d8e1
the_dise <dmitry.dise.gavrilov@gmail.com>
2024-09-30 19:20:17 +0300

    [tmux] separated variables from the script. deleted comments

--- tmux/custom/proxy.sh
+++ tmux/custom/proxy.sh
@@ -1,16 +1,9 @@
-
-# If this module depends on an external Tmux plugin, say so in a comment.
-# E.g.: Requires https://github.com/aaronpowell/tmux-weather
-
-show_proxy() { # This function name must match the module name!
+show_proxy() { 
   local index icon color text module
   
-  local proxy_host="127.0.0.1"
-  local proxy_port="2080"
+  index=$1 
 
-  index=$1 # This variable is used internally by the module loader in order to know the position of this module
-
-  if nc -z $proxy_host $proxy_port; then
+  if nc -z $PROXY_HOST $PROXY_PORT; then
     icon="$(  get_tmux_option "@catppuccin_proxy_icon"  "󰞉" )"
     color="$( get_tmux_option "@catppuccin_proxy_color" "$thm_cyan" )"
     text="$(  get_tmux_option "@catppuccin_proxy_text"  "On" )"
